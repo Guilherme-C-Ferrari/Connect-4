f2f04d568a904081b2cb8539faf0f3371a463741
Guilherme Carvalho Ferrari <guicferrari@hotmail.com>
2026-04-27 16:05:16 -0300

    Construção de funções base do tabuleiro

--- Scripts/Tabuleiro.gd
+++ Scripts/Tabuleiro.gd
@@ -1,2 +1,68 @@
 extends Resource
 class_name Tabuleiro
+
+const JOGADOR_AMARELO = "A"
+const JOGADOR_VERMELHO = "V"
+const SEM_JOGADA = ""
+
+@export_storage var tabuleiro: Array
+@export_storage var jogador: String
+
+func _init():
+	self.tabuleiro = [
+		[SEM_JOGADA, SEM_JOGADA, SEM_JOGADA, SEM_JOGADA, SEM_JOGADA, SEM_JOGADA, SEM_JOGADA],
+		[SEM_JOGADA, SEM_JOGADA, SEM_JOGADA, SEM_JOGADA, SEM_JOGADA, SEM_JOGADA, SEM_JOGADA],
+		[SEM_JOGADA, SEM_JOGADA, SEM_JOGADA, SEM_JOGADA, SEM_JOGADA, SEM_JOGADA, SEM_JOGADA],
+		[SEM_JOGADA, SEM_JOGADA, SEM_JOGADA, SEM_JOGADA, SEM_JOGADA, SEM_JOGADA, SEM_JOGADA],
+		[SEM_JOGADA, SEM_JOGADA, SEM_JOGADA, SEM_JOGADA, SEM_JOGADA, SEM_JOGADA, SEM_JOGADA],
+		[SEM_JOGADA, SEM_JOGADA, SEM_JOGADA, SEM_JOGADA, SEM_JOGADA, SEM_JOGADA, SEM_JOGADA],
+	]
+	self.jogador = JOGADOR_AMARELO
+	
+func alternar_jogador() -> void:
+	self.jogador = JOGADOR_AMARELO if self.jogador == JOGADOR_VERMELHO else JOGADOR_VERMELHO
+
+func empate() -> bool:
+	for linha in self.tabuleiro:
+		for coluna in linha:
+			if coluna == SEM_JOGADA:
+				return false
+	return true
+
+func avaliar() -> float:
+	var eval: float = 0.5
+	pass
+	return eval 
+
+func jogador_atual() -> String:
+	return jogador
+
+func jogadas_possiveis() -> Array:
+	var jogadas: Array = []
+	for coluna in range(7):
+		if tabuleiro[0][coluna] == SEM_JOGADA:
+			jogadas.append(coluna)
+	return jogadas
+
+func movimentar(movimento: int, jogador: String) -> Tabuleiro:
+	var novo_tabuleiro = self.duplicate(true)
+	for linha in range(5, -1, -1):
+		if novo_tabuleiro.tabuleiro[linha][movimento] == SEM_JOGADA:
+			novo_tabuleiro.tabuleiro[linha][movimento] = jogador
+			break
+	return novo_tabuleiro
+
+func valida_jogada(coluna: int) -> bool:
+	if tabuleiro[0][coluna] == SEM_JOGADA:
+		return true
+	else:
+		return false
+
+func jogada(coluna: int) -> bool:
+	if valida_jogada(coluna):
+		for linha in range(5, -1, -1):
+			if tabuleiro[linha][coluna] == SEM_JOGADA:
+				tabuleiro[linha][coluna] = jogador
+				alternar_jogador()
+				return true
+	return false
