ffb79005f84f13f207ebaf3540dc3d09a42d015a
Guilherme Carvalho Ferrari <guicferrari@hotmail.com>
2026-05-05 15:55:57 -0300

    Direcionamento dos botões para funcionalidade

--- Scripts/Connect_4.gd
+++ Scripts/Connect_4.gd
@@ -2,7 +2,6 @@ extends Node2D
 
 @onready var botoes = $TabuleiroFisico/HBoxContainer.get_children()
 @onready var label_resultado = $CanvasLayer/Menu/%LabelPrincipal
-@onready var botao_jogo = $CanvasLayer/Menu/%BotaoMenu1
 
 var ficha = preload("res://Cenas/ficha.tscn")
 var tabuleiro_logico = preload("res://Scripts/TabuleiroLogico.gd")
@@ -57,7 +56,6 @@ func jogada_maquina():
 func fim_jogo(resultado: String) -> void:
 	label_resultado.text = resultado
 	desabilitar_botoes()
-	botao_jogo.text = "Novo Jogo"
 
 func habilitar_botoes() -> void:
 	for i in range(botoes.size()):
@@ -70,15 +68,16 @@ func desabilitar_botoes() -> void:
 		botao.disabled = true
 
 func reset_game() -> void:
+	label_resultado.text = ""
 	jogo = Tabuleiro.new()
 	get_tree().call_group("Fichas", "queue_free")
 	get_tree().call_group("Colunas", "reset")
 	habilitar_botoes()
 
-func _on_button_pressed() -> void:
-	if botao_jogo.text == "Novo Jogo":
-		# botao_jogo.text = "Jogada IA"
-		label_resultado.text = ""
+func _on_button_pressed(botao: Button) -> void:
+	if botao.name == "BotaoNovoJogo":
 		reset_game()
-	else:
-		jogada_maquina()
+	elif botao.name == "BotaoJogadaIA":
+		print("Jogada IA")
+	elif botao.name == "BotaoPartidaIA":
+		print("Partida De IAs")
--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -1,5 +1,10 @@
 extends Control
-signal menu_botao_pressionado
+signal menu_botao_pressionado(botao: Button)
 
-func _on_button_pressed() -> void:
-	emit_signal("menu_botao_pressionado")
+func _ready() -> void:
+	for botao in $MarginContainer/VBoxContainer.get_children():
+		if botao is Button:
+			botao.pressed.connect(_on_button_pressed.bind(botao))
+
+func _on_button_pressed(botao: Button) -> void:
+	emit_signal("menu_botao_pressionado", botao)
